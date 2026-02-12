35a1c6552ff86441152898d171ce4635e9443ff2
Bertrand FOVEZ <bertrand.fovez@gmail.com>
2026-02-12 15:55:03 +0100

    wip

--- api/index.js
+++
@@ -1,7 +0,0 @@
-import { HttpLayerRouter } from "@effect/platform";
-import { MainLayer } from "./toto";
-const { dispose, handler } = HttpLayerRouter.toWebHandler(MainLayer);
-process.on("SIGTERM", () => {
-    dispose();
-});
-export default { fetch: handler };
