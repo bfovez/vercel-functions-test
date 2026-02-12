2f8358a50dd1c31bd5b91eb151195d47f069b12f
Bertrand FOVEZ <bertrand.fovez@gmail.com>
2026-02-12 10:07:11 +0100

    wip

--- vercel.ts
+++ vercel.ts
@@ -1,9 +1,9 @@
 import { routes, type VercelConfig } from "@vercel/config/v1";
 
 export const config: VercelConfig = {
-  functions: {
-    "api/*": { },
-  },
+  // functions: {
+  //   "api/*": { supportsCancellation: true },
+  // },
   regions: ["cdg1"],
   rewrites: [routes.rewrite("/api/(.*)", "/api")],
 };
