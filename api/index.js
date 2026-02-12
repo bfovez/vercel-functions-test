89df849c5f584bac444deccfebb534f673b78b57
Bertrand FOVEZ <bertrand.fovez@gmail.com>
2026-02-12 15:48:39 +0100

    wip

---
+++ api/index.js
@@ -0,0 +1,7 @@
+import { HttpLayerRouter } from "@effect/platform";
+import { MainLayer } from "./toto";
+const { dispose, handler } = HttpLayerRouter.toWebHandler(MainLayer);
+process.on("SIGTERM", () => {
+    dispose();
+});
+export default { fetch: handler };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-// import { HttpLayerRouter } from "@effect/platform"
-// import { MainLayer } from "./toto.ts"
+import { HttpLayerRouter } from "@effect/platform"
+import { MainLayer } from "./toto"
 
-// const { dispose, handler } = HttpLayerRouter.toWebHandler(MainLayer)
+const { dispose, handler } = HttpLayerRouter.toWebHandler(MainLayer)
 
-// process.on("SIGTERM", () => {
-// 	dispose()
-// })
+process.on("SIGTERM", () => {
+  dispose()
+})
 
-// export default { fetch: handler }
-
-export default {
-  fetch(request: Request) {
-    return new Response('Hello from Vercel!');
-  },
-};
+export default { fetch: handler }
